d2e2b4ff9919647a65cae51c51f3ca61bfabef2c
karthi9772 <karthi.g9772@gmail.com>
2025-05-07 01:25:05 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-app.listen(3000, () => {
-  console.log("in 3000");
+
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("server is running");
+  return res.status(200).json({ message: "server is running" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
